dc80b8ff81e684a871b4ed08ebc72b6cfbff13c8
mariaisabelalvarez <isabelalvarez@uchicago.edu>
2023-01-13 19:50:40 -0600

    Write In Data
    Here, I am writing in my dataset into an R Script with the read_excel function.

---
+++ WriteInDataCCRollCallVotesTotal.R
@@ -0,0 +1,8 @@
+#ReadInData 
+
+#Read In Complete Data Set with all of the roll call votes in the Constitutional Assembly in binary form 
+totalCCrollcall <- read_excel("~/Downloads/d2mCCRollCallVotesTotal.xlsx")
+
+
+
+
