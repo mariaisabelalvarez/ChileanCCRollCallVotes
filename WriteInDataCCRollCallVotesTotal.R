eac20b4cc80a7f57d66ce537e2e0019ca9bfe2c4
mariaisabelalvarez <isabelalvarez@uchicago.edu>
2023-01-17 12:12:35 -0600

    Wrote in the Data
    First, I wrote in my original data spreadsheet with the readr function for excel. Then, as a test run, I wrote in a test csv file to see if the function was working. Externally, created a separate spreadsheet of my data in Excel by saving as a csv file. But I had trouble with the readr csv function because my dataset was too large. I'll also write this down in an issue. @nrdowling @hjennybo

--- WriteInDataCCRollCallVotesTotal.R
+++ WriteInDataCCRollCallVotesTotal.R
@@ -3,6 +3,9 @@
 #Read In Complete Data Set with all of the roll call votes in the Constitutional Assembly in binary form 
 totalCCrollcall <- read_excel("~/Downloads/d2mCCRollCallVotesTotal.xlsx")
 
+#Read In test.csv 
+testsheet <- read_csv("~/Downloads/test.csv")
 
-
-
+#Read In testdata.csv
+datacsv <- read_csv("~/Downloads/testdata.csv")
+Sys.setenv("VROOM_CONNECTION_SIZE")
