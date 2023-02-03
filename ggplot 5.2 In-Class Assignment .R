de1610ddd4e87a7b32a3910a7ac95187c2056a78
mariaisabelalvarez <isabelalvarez@uchicago.edu>
2023-02-02 20:23:16 -0600

    5.2 In-Class Assignment
    Script containing the ggplot 5.2 In-Class Assignment plots. @hjennybo @nrdowling

---
+++ ggplot 5.2 In-Class Assignment .R
@@ -0,0 +1,35 @@
+#5.2 In Class Assignment 
+
+library(munsell)
+
+mdata <- read_csv("Documents/GitHub/d2m-2023-shared/datasets/MM Data.csv")
+
+mmdata.long <- read_csv(("Documents/GitHub/d2m-2023-shared/datasets/MM Data.csv"))%>%
+  pivot_longer(cols = c("Red", "Green", "Blue", "Orange","Yellow", "Brown"),names_to = "Color", values_to = "Number")
+
+#PLOTS
+
+#Plot 1 
+ggplot(mmdata.long, aes(x=Color, y=Number, color= Color, fill=Color)) + 
+  geom_violin(alpha = 0.3)+ 
+  scale_fill_manual(values=c("blue", "brown","green","orange","red","yellow")) +
+  geom_jitter(mapping=aes(color=Color))+
+  theme_classic() +
+  scale_color_manual(breaks = c("Blue", "Brown", "Green","Orange", "Red","Yellow"),
+                     values=c("blue", "brown","green","orange","red","yellow")) 
+
+
+#Plot 2 (Challenge)
+ggplot(mmdata.long, aes(x= Color, y= Number, fill= Color)) +
+  geom_boxplot() +
+  scale_x_discrete(limits= c("Blue", "Brown", "Green","Orange", "Red","Yellow"))+
+  geom_violin(aes(fill=Color, color= Color), alpha=0.2) +
+  scale_fill_manual(breaks = c("Blue", "Brown", "Green","Orange", "Red","Yellow"),
+                    values = c("blue","brown", "green", "orange", "red", "yellow")) +
+  geom_jitter(mapping=aes(fill= Color, color= Color)) +
+  theme_classic() +
+  scale_color_manual(breaks = c("blue","brown", "green", "orange", "red", "yellow"),
+                     values = c("blue","brown", "green", "orange", "red", "yellow"))
+  
+  
+
